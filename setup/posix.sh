64beb3d51687d8a2036145dff40e232f39093c9b
Alexis BRENON <brenon.alexis@gmail.com>
2018-08-02 07:18:29 +0200

    🚧  Modularize setup

--- setup/posix.sh
+++ setup/posix.sh
@@ -1,143 +1,82 @@
 #! /bin/sh
 
-f_prompt_alexis_posix_setup() {
-  mkdir -p "${XDG_CACHE_DIR:-${HOME}/.cache}"
-  mkdir -p "${XDG_CACHE_DIR:-${HOME}/.cache}/prompt_alexis"
+f_sterope_posix_setup() {
+    v_sterope_cache_dir="${XDG_CACHE_DIR:-${HOME}/.cache}/sterope"
+    v_sterope_config_dir="${XDG_CONFIG_DIR:-${HOME}/.config}/sterope"
 
-  # Load utilities functions
-  . "${PROMPT_ALEXIS_PATH}/functions/posix.sh"
-  if [ -z "${v_prompt_alexis_git_symbols:-""}" ]; then
+    mkdir -p "${v_sterope_cache_dir}"
+    mkdir -p "${v_sterope_config_dir}"
+
+    v_sterope_git_symbols="$(lf_sterope_get_git_symbols)\\n"
+    v_sterope_py_symbols="$(lf_sterope_get_py_symbols)\\n"
+    v_sterope_help_message="$(lf_sterope_get_help_message)\\n"
+
+    printf '%s' "${v_sterope_git_symbols}" #| sed -e 's/:\(.*\)/:'"'"'\1'"'"'/'
+    printf '%s' "${v_sterope_py_symbols}" # | sed -e 's/:\(.*\)/:'"'"'\1'"'"'/'
+    printf '%s' "${v_sterope_help_message}"
+}
+
+lf_sterope_get_git_symbols() {
+    v_tmp=""
     if [ -e "$HOME/.local/share/icons-in-terminal/icons_bash.sh" ]; then
-      DEBUG "Loading icons in terminal"
-      . "$HOME/.local/share/icons-in-terminal/icons_bash.sh"
-      v_tmp=" ${oct_octoface:-"octoface"}    " # is_a_git_repo
-      v_tmp="$v_tmp:${powerline_left_hard_divider:-"left divider"} " # separator
-      v_tmp="$v_tmp: : : "
-      v_tmp="$v_tmp:${fa_asterisk:-"asterisk"} :${fa_eye_slash:-"eye_slash"} "
-      v_tmp="$v_tmp: : : "
-      v_tmp="$v_tmp:${fa_pencil:-pencil} :${fa_minus:-"minus"} : : "
-      v_tmp="$v_tmp: "
-      v_tmp="$v_tmp:${fa_pencil} :${fa_mail_reply:-"mail_reply"} :✨ :🔥 : "
-      v_tmp="$v_tmp:${oct_git_commit:-"commit"} :${fa_wrench:-"wrench"} : : : "
-      v_tmp="$v_tmp:${fa_chain_broken:-"chain_broken"} :${md_cloud_off:-"cloud_off"} : : : "
-      v_tmp="$v_tmp:${oct_repo_forked:-repo_forked} :${md_fast_forward:-fast_forward} :${oct_cloud_upload:-cloud_upload} : : "
-      v_tmp="$v_tmp:${oct_git_pull_request:-pull_request} :${oct_git_merge:-merge} : : : "
-      v_tmp="$v_tmp:${fa_tag:-tag} "
-      v_prompt_alexis_git_symbols="${v_tmp}"
-      unset v_tmp
+        DEBUG "Loading icons in terminal"
+        #shellcheck disable=1090
+        . "$HOME/.local/share/icons-in-terminal/icons_bash.sh"
     else
-      echo "prompt_alexis: Git status line rely on icons-in-terminal project for nice icons." >&2
-      echo "Please see https://github.com/sebastiencs/icons-in-terminal/." >&2
-      v_prompt_alexis_git_symbols=""
+        WARN "Sterope prompt heavily relies on icons-in-terminal"
+        WARN "Please see https://github.com/sebastiencs/icons-in-terminal/"
     fi
-  fi
-  DEBUG "git_symbols: '${v_prompt_alexis_git_symbols}'"
-  v_prompt_alexis_git_symbols_map="
-    is_a_git_repo:1
-    separator:2
-    has_stashes:6
-    has_untracked:7
-    has_workspace_mods:11
-    has_workspace_dels:12
-    has_index_mods:16
-    has_index_moves:17
-    has_index_adds:18
-    has_index_dels:19
-    ready_to_commit:21
-    has_pending_action:22
-    detached:26
-    not_tracked:27
-    has_diverged:31
-    can_ff:32
-    should_push:33
-    rebase_tracking:36
-    merge_tracking:37
-    tag:41"
+    v_tmp="is_a_git_repo:${oct_octoface:-git}    \\n"
+    v_tmp="${v_tmp}separator:${powerline_left_hard_divider:-▓▒░} \\n"
+    v_tmp="${v_tmp}has_stashes:${fa_asterisk:-stash} \\n"
+    v_tmp="${v_tmp}has_untracked:${fa_eye_slash:-untracked} \\n"
+    v_tmp="${v_tmp}has_conflicts:\\u1F4A5 \\n"  # Explosion
+    v_tmp="${v_tmp}has_pending_action:\\u1F527 \\n"  # Wrench
+    v_tmp="${v_tmp}has_workspace_mods:${fa_pencil:-M} \\n"
+    v_tmp="${v_tmp}has_workspace_dels:${fa_minus:-D} \\n"
+    v_tmp="${v_tmp}has_index_mods:${fa_pencil:-M} \\n"
+    v_tmp="${v_tmp}has_index_moves:${fa_mail_reply:-V} \\n"
+    v_tmp="${v_tmp}has_index_adds:${fa_plus:-A} \\n"
+    v_tmp="${v_tmp}has_index_dels:${fa_minus:-D} \\n"
+    v_tmp="${v_tmp}ready_to_commit:${oct_git_commit:-\\u1F3C1} \\n"
+    v_tmp="${v_tmp}detached:${fa_chain_broken:-detached} \\n"
+    v_tmp="${v_tmp}not_tracked:${md_cloud_off:-not tracked} \\n"
+    v_tmp="${v_tmp}has_diverged:${oct_repo_forked:-Y} \\n"
+    v_tmp="${v_tmp}can_ff:${md_fast_forward:-">>"} \\n"
+    v_tmp="${v_tmp}should_push:${oct_cloud_upload:-"->"} \\n"
+    v_tmp="${v_tmp}tag:${fa_tag:-tag} \\n"
+    printf '%s' "${v_tmp}"
+}
 
-  if [ -z "${v_prompt_alexis_py_symbols:-""}" ]; then
+lf_sterope_get_py_symbols() {
+    v_tmp=""
     if [ -e "$HOME/.local/share/icons-in-terminal/icons_bash.sh" ]; then
-      DEBUG "Loading icons in terminal"
-      . "$HOME/.local/share/icons-in-terminal/icons_bash.sh"
-      v_tmp=" ${dev_python:-"python"}  " # is a virtualenv
-      v_tmp="$v_tmp:${powerline_left_hard_divider:-"left divider"} " # separator
-      v_prompt_alexis_py_symbols="${v_tmp}"
-      unset v_tmp
+        DEBUG "Loading icons in terminal"
+        #shellcheck disable=1090
+        . "$HOME/.local/share/icons-in-terminal/icons_bash.sh"
     else
-      echo "prompt_alexis: Virtualenv status line rely on icons-in-terminal project for nice icons." >&2
-      echo "Please see https://github.com/sebastiencs/icons-in-terminal/." >&2
-      v_prompt_alexis_py_symbols=""
+        WARN "Sterope prompt heavily relies on icons-in-terminal"
+        WARN "Please see https://github.com/sebastiencs/icons-in-terminal/"
     fi
-  fi
-  DEBUG "py_symbols:  '${v_prompt_alexis_py_symbols}'"
-  v_prompt_alexis_py_symbols_map="
-    is_a_virtualenv:1
-    separator:2"
 
-  v_prompt_alexis_help_string="alexis prompt is a multiline prompt which display:
+    v_tmp="is_a_virtualenv:${dev_python:-py}    \\n"
+    v_tmp="${v_tmp}separator:${powerline_left_hard_divider:-▓▒░} \\n"
+    printf '%s' "${v_tmp}"
+}
+
+lf_sterope_get_help_message() {
+    cat << EOM
+Sterope is a multiline prompt which display:
   * an empty line to separate one command from another;
-  * a git line if you are in a git repository (see below);
-  * a virtualenv python line (see below);
+  * a git line if you are in a git repository;
+  * a virtualenv python line in a VirtualEnv;
   * the host and working directory line (as classical prompts);
   * an indicator of the exit status of the last command.
 
 The git line is based on the god-bless-git plugin (github.com/AlexisBRENON/god-bless-git) and is similar to the oh-my-git project. It's a colorful line with plenty informations.
-
-List of the prompt options:
-  -s <symbols>
-    A colon separated string representing symbols to use in the git line.
-    Default:
-      \"${v_prompt_alexis_git_symbols}\"
-    Symbols meanings are:
-      1: Current directory is a git repository
-      2: In-line separator
-      6: Has stashes
-      7: Has untracked files
-      11: Workspace is modified
-      12: Workspace has deleted files
-      16: Index has modified files
-      17: Index has moved files
-      18: Index has new files
-      19: Index has deleted files
-      21: The repo is ready to commit (all in the index)
-      22: An operation (merge, rebase) is pending
-      26: HEAD is detached
-      27: Current branch as no upstream
-      31: Local and upstream have diverged
-      32: A fast-forward merge is possible
-      33: Your local branch is ahead of the upstream
-      36: Pull will rebase
-      37: Pull will merge
-      41: HEAD is tagged
-
-  -p <symbols>
-    A colon separated string representing symbols to use in the Python line.
-    Default:
-      \"${v_prompt_alexis_py_symbols}\"
-    Symbols meanings are:
-      1: Virtualenv is activated
-      2: In-line separator"
-
-  l_prompt_alexis_old_optind=${OPTIND}
-  while getopts s:p: OPTION; do
-    case ${OPTION} in
-      s)
-        v_prompt_alexis_git_symbols="${OPTARG}"
-        ;;
-      p)
-        v_prompt_alexis_py_symbols="${OPTARG}"
-        ;;
-    esac
-  done
-  OPTIND=${l_prompt_alexis_old_optind}
-  unset l_prompt_alexis_old_optind
-
-  export v_prompt_alexis_git_symbols
-  export v_prompt_alexis_git_symbols_map
-  export v_prompt_alexis_py_symbols
-  export v_prompt_alexis_py_symbols_map
-  export v_prompt_alexis_help_string
-  export v_prompt_alexis_log_file="/tmp/prompt_alexis_${USER}_log"
+EOM
 }
+
 
 f_prompt_alexis_posix_help() {
   echo "${v_prompt_alexis_help_string}"
