c0fdf14764e0af42f688b80fe9749035499bd3d3
Alexis BRENON <brenon.alexis@gmail.com>
2018-08-07 19:20:16 +0200

    🚧  Add ZSH setup

--- setup/posix.sh
+++ setup/posix.sh
@@ -11,20 +11,20 @@ f_sterope_posix_setup() {
     v_sterope_py_symbols="$(lf_sterope_get_py_symbols)\\n"
     v_sterope_help_message="$(lf_sterope_get_help_message)\\n"
 
-    printf '%s' "${v_sterope_git_symbols}" #| sed -e 's/:\(.*\)/:'"'"'\1'"'"'/'
-    printf '%s' "${v_sterope_py_symbols}" # | sed -e 's/:\(.*\)/:'"'"'\1'"'"'/'
-    printf '%s' "${v_sterope_help_message}"
+    info "${v_sterope_git_symbols}" #| sed -e 's/:\(.*\)/:'"'"'\1'"'"'/'
+    info "${v_sterope_py_symbols}" # | sed -e 's/:\(.*\)/:'"'"'\1'"'"'/'
+    info "${v_sterope_help_message}"
 }
 
 lf_sterope_get_git_symbols() {
     v_tmp=""
     if [ -e "$HOME/.local/share/icons-in-terminal/icons_bash.sh" ]; then
-        DEBUG "Loading icons in terminal"
+        debug "Loading icons in terminal"
         #shellcheck disable=1090
         . "$HOME/.local/share/icons-in-terminal/icons_bash.sh"
     else
-        WARN "Sterope prompt heavily relies on icons-in-terminal"
-        WARN "Please see https://github.com/sebastiencs/icons-in-terminal/"
+        warn "Sterope prompt heavily relies on icons-in-terminal"
+        warn "Please see https://github.com/sebastiencs/icons-in-terminal/"
     fi
     v_tmp="is_a_git_repo:${oct_octoface:-git}    \\n"
     v_tmp="${v_tmp}separator:${powerline_left_hard_divider:-▓▒░} \\n"
@@ -51,12 +51,12 @@ lf_sterope_get_git_symbols() {
 lf_sterope_get_py_symbols() {
     v_tmp=""
     if [ -e "$HOME/.local/share/icons-in-terminal/icons_bash.sh" ]; then
-        DEBUG "Loading icons in terminal"
+        debug "Loading icons in terminal"
         #shellcheck disable=1090
         . "$HOME/.local/share/icons-in-terminal/icons_bash.sh"
     else
-        WARN "Sterope prompt heavily relies on icons-in-terminal"
-        WARN "Please see https://github.com/sebastiencs/icons-in-terminal/"
+        warn "Sterope prompt heavily relies on icons-in-terminal"
+        warn "Please see https://github.com/sebastiencs/icons-in-terminal/"
     fi
 
     v_tmp="is_a_virtualenv:${dev_python:-py}    \\n"
@@ -76,67 +76,4 @@ Sterope is a multiline prompt which display:
 The git line is based on the god-bless-git plugin (github.com/AlexisBRENON/god-bless-git) and is similar to the oh-my-git project. It's a colorful line with plenty informations.
 EOM
 }
-
-
-f_prompt_alexis_posix_help() {
-  echo "${v_prompt_alexis_help_string}"
-}
-
-f_prompt_alexis_posix_preview() {
-  (
-    f_prompt_alexis_posix_setup
-    (
-      echo 'alexis theme, not in a git repo, after a successful command:'
-      v_prompt_alexis_git_line=""
-      v_prompt_alexis_py_line=""
-      f_prompt_alexis_separating_line
-      f_prompt_alexis_host_info
-      f_prompt_alexis_pwd
-      v_prompt_alexis_last_command_status=0
-      f_prompt_alexis_last_command_status
-      f_prompt_alexis_print_prompt
-      echo "command arg1 arg2 ... argn"
-      echo ""
-    )
-    (
-    echo 'alexis theme, in a git repo, after a failed command:'
-    export gbg_is_a_git_repo='true'
-    export gbg_repo_has_stashes='true'
-    export gbg_workspace_deletions_num=1
-    export gbg_workspace_has_deletions="true"
-    export gbg_workspace_modifications_num=1
-    export gbg_workspace_has_modifications="true"
-    export gbg_workspace_has_untracked="true"
-    export gbg_index_has_additions="true"
-    export gbg_index_additions_num=1
-    export gbg_upstream_commits_ahead_num=1
-    export gbg_upstream_commits_has_ahead="true"
-    export gbg_upstream_commits_behind_num=1
-    export gbg_upstream_commits_has_behind="true"
-    export gbg_upstream_has_diverged="true"
-    export gbg_upstream_has_upstream="true"
-    export gbg_upstream_name="origin/branchname"
-    export gbg_head_branch="localbranchname"
-    export gbg_head_hash="commit hash"
-    export gbg_head_is_on_tag="true"
-    export gbg_head_tag="tagname"
-    v_prompt_alexis_git_line="$(f_prompt_alexis_build_git_line)"
-    if [ -n "${v_prompt_alexis_git_line}" ]
-    then
-      v_prompt_alexis_git_line="${v_prompt_alexis_git_line}\n"
-    fi
-    f_prompt_alexis_separating_line
-    f_prompt_alexis_host_info
-    f_prompt_alexis_pwd
-    v_prompt_alexis_last_command_status=1
-    f_prompt_alexis_last_command_status
-    f_prompt_alexis_print_prompt
-    echo "command arg1 arg2 ... argn"
-    echo ''
-    )
-  )
-}
-
-prompt_alexis_help() { f_prompt_alexis_posix_help "$@"; }
-prompt_alexis_preview() { f_prompt_alexis_posix_preview "$@"; }
 
--- setup/zsh.sh
+++ setup/zsh.sh
@@ -1,39 +1,22 @@
 #! /usr/bin/env zsh
 
-function f_prompt_alexis_zsh_setup {
-  . "${PROMPT_ALEXIS_PATH}/functions/zsh.sh"
+function f_sterope_zsh_setup {
   prompt_opts=(cr percent subst)
 
-  v_prompt_alexis_no_async="false"
-  l_prompt_alexis_old_optind=${OPTIND}
-  while getopts n OPTION; do
-    case ${OPTION} in
-      n)
-        v_prompt_alexis_no_async="true"
-        ;;
-    esac
-  done
-  OPTIND=${l_prompt_alexis_old_optind}
-  unset l_prompt_alexis_old_optind
-
-  v_prompt_alexis_help_string="${v_prompt_alexis_help_string}
-  -n
-    Do not use the async feature for git info line."
+  v_sterope_no_async="false"
 
   # Load required functions.
   autoload -Uz add-zsh-hook
-  add-zsh-hook precmd prompt_alexis_zsh_build_prompt
+  # Build the prompt on precmd hook
+  add-zsh-hook precmd sterope_zsh_build_prompt
 
-  PROMPT='${$(f_prompt_alexis_print_prompt)}${editor_info[keymap]}'
-  RPROMPT=''
+  # Just print the prompt (do not build it)
+  PROMPT='${$(f_sterope_print_prompt)}${editor_info[keymap]}'
+  RPROMPT='${$(f_sterope_print_rprompt)}'
 
-  export v_prompt_alexis_no_async
-  export v_prompt_alexis_async_git_data="/tmp/prompt_alexis-${USER}-git"
-  export v_prompt_alexis_help_string
-}
-
-# TODO move it to a posix version
-function f_prompt_alexis_zsh_preview {
-  prompt_preview_safely 'alexis'
+  export v_sterope_no_async
+  # Where async data are stored for callback
+  export v_sterope_async_git_data="/tmp/prompt_alexis-${USER}-git"
+  export v_sterope_help_message
 }
 
