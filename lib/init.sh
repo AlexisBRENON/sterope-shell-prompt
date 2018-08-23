e1d70df0ad0c812649550fb7961731cc4cbe22c0
Alexis BRENON <brenon.alexis@gmail.com>
2018-08-23 22:44:29 +0200

    🚑  Fix infinite loop for unamed shells

--- lib/init.sh
+++ lib/init.sh
@@ -4,12 +4,14 @@
 . "${STEROPE_ROOT}/lib/posix/init.sh"
 
 current_shell=$(get_current_shell)
-shell_lib="${STEROPE_ROOT}/lib/${current_shell}/init.sh"
-if [ -e "${shell_lib}" ]; then
-    # shellcheck source=./lib/bash/init.sh
-    # shellcheck source=./lib/zsh/init.sh
-    . "${shell_lib}"
-else
-    WARN "Sterope is not implemented yet for ${current_shell} shell"
+if [ -n "${current_shell}" ]; then
+    shell_lib="${STEROPE_ROOT}/lib/${current_shell}/init.sh"
+    if [ -e "${shell_lib}" ]; then
+        # shellcheck source=./lib/bash/init.sh
+        # shellcheck source=./lib/zsh/init.sh
+        . "${shell_lib}"
+    else
+        WARN "Sterope is not implemented yet for ${current_shell} shell"
+    fi
 fi
 
